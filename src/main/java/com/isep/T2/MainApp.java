4fa4c66fb37caa265ae1f20397f6de12bd6feca5
marin.mouscadet <marin.mouscadet@ibm.com>
2019-03-29 12:53:50 +0100

    Q2 done

--- src/main/java/com/isep/T2/MainApp.java
+++ src/main/java/com/isep/T2/MainApp.java
@@ -1,15 +1,27 @@
 package com.isep.T2;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import org.apache.camel.main.Main;
+import org.apache.camel.util.json.JsonObject;
 
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 import com.rabbitmq.client.Channel;
+
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.util.Date;
 
 /**
  * A Camel Application
@@ -22,29 +34,57 @@ public class MainApp {
     public static void main(String... args) throws Exception {
     	
     	FindFileInDirectory FindFiles = new FindFileInDirectory();
-    	ArrayList<String> Files = FindFiles.findFile("src/main/resources/articles");
+    	ArrayList<String> Files = FindFiles.findFile("src/main/resources/articlesPending");
     	
     	ConnectionFactory factory = new ConnectionFactory();
       	factory.setHost("localhost"); //Set the host
       	Connection connection = factory.newConnection();
    	    Channel channel = connection.createChannel();
+   	    channel.queueDelete(QUEUE_NAME); //Delete the queue to avoid multi-answered
    	    channel.exchangeDeclare(EXCHANGE_NAME, "direct", true); //Declare the Exchange 
       	channel.queueDeclare(QUEUE_NAME, true, false, false, null); //Declare the Queue name
       	channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "camel"); //Bind the Queue
+      	
     	
-    	ArrayList<JSONObject> ListFile = new ArrayList<JSONObject>() ; //Créer l'objet à envoyer
     	
+      	Files.forEach(name -> {
+      			JSONObject file = ReadJsonFile.read(name); //Put the file in a JSON Object
+            	try {
+    				channel.basicPublish("", QUEUE_NAME, null, file.toString().getBytes("UTF-8")); //Send the message
+    			} catch (UnsupportedEncodingException e) {
+    				e.printStackTrace();
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+      		});
+      	
+    	DeliverCallback deliverCallback = (RecupFile, messageRecup) -> { //Recevied the msg
+    	    String message = new String(messageRecup.getBody(), "UTF-8");
+    	    com.google.gson.JsonObject jsonObject = new JsonParser().parse(message).getAsJsonObject();
+    	    
+    	    Date date = new Date(); //Create a date
+    	    SimpleDateFormat ft = new SimpleDateFormat ("y-MM-d"); //Format the date
+  
+    	    jsonObject.addProperty("accepted_date", ft.format(date)); //Add the acceptedDate to the JSON
+    	    
+    	    String reviewer_name = "Computer_1"; //Create the Reviewer name
+    	    jsonObject.addProperty("reviewer", reviewer_name); //Add it to the JSON
+    	    
+    	    String title = jsonObject.get("title").toString(); //Find the title
+    	    title = title.substring(1, title.length()-1); //Erase quotation mark
+    	    
+    	    //Write the file
+    	    try (FileWriter file = new FileWriter("src/main/resources/articlesAccepted/" + title + ".json")) {
+                file.write(jsonObject.toString());
+                file.flush();
+     
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    	    
+    	};
     	
-    	Files.forEach((name) -> {
-        	JSONObject file = ReadJsonFile.read(name); //Put the file in a JSON Object
-        	try {
-				channel.basicPublish("", QUEUE_NAME, null, file.toString().getBytes("UTF-8")); //Send the message
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-    	});
+    	channel.basicConsume(QUEUE_NAME, true, deliverCallback, RecupFile -> { }); //Allow the delivering
     }
 }
 
--- src/main/java/com/isep/T2/ReadJsonFile.java
+++ src/main/java/com/isep/T2/ReadJsonFile.java
@@ -20,7 +20,7 @@ public class ReadJsonFile {
     }
 	
 	public static JSONObject read(String name) {
-		File file = new File("src/main/resources/articles/" + name);
+		File file = new File("src/main/resources/articlesPending/" + name);
 		JSONObject article = new JSONObject();
 
 		try {
@@ -30,6 +30,7 @@ public class ReadJsonFile {
 		    int lineNum = 0;
 		    while (scanner.hasNextLine()) {
 		        String line = scanner.nextLine();
+		        line = line.replaceAll("[\"'\u2018\u2019\u201c\u201d]", "");
 		        lineNum++;
 		        if(line.substring(0, 10).contains("title")) { 
 		            article.put("title", line.substring(7));
@@ -44,6 +45,7 @@ public class ReadJsonFile {
 		            article.put("content", line.substring(9));
 		        }
 		    }
+		    
 		    return article;
 		} catch(FileNotFoundException e) { 
 		    //handle this
